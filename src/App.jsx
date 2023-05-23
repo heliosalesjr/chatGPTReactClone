6c7e6d479ea946109059a13392c3b1fbefac14d9
heliosalesjr <heliosalesjr@gmail.com>
2023-05-23 13:15:38 -0300

    version 01

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+import React from 'react'
+
+const App = () => {
+  const [ value, setValue ] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+  }
+
+  const getMessage = async () => {
+    const options = {
+      method: 'POST',
+      body : JSON.stringify({
+        message : value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+
+    }
+  
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      
+      setMessage(data.choices[0].message)
+
+    } catch (error) {
+      console.log(error);
+    };
+  }
+
+  useEffect(() => {
+    if (!currentTitle && value  && message) {
+      setCurrentTitle(value)
+    }
+
+    if (currentTitle && value && message) {
+      setPreviousChats( (prevChats => [...prevChats, 
+        {
+          title: currentTitle, 
+          role: 'user',
+          content: value
+        },
+
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        }
+      ]))
+    }
+  }, [message, currentTitle])
+
+    console.log(message)
+
+    const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+    const uniqueTitles = Array.from (new Set(previousChats.map(previousChat => previousChat.title)))
+
+  return (
+
+    <div className='app'>
+
+      <section className='side-bar'>
+
+        <button onClick={createNewChat}>+ new chat</button>
+
+        <ul className='history'>
+          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+        </ul>
+          
+        <nav>
+          <p>Made by Helin</p>
+        </nav>
+
+      </section>
+
+      <section className='main'> 
+      
+        {!currentTitle && <h1>HelioGPT</h1>}
+
+        <ul className='feed'>
+          {currentChat.map((chatMessage, index) => <li key={index}>
+            <p className='role'> {chatMessage.role} </p>
+            <p> {chatMessage.content} </p>
+          </li>)}
+
+        </ul>
+
+        <div className="bottom-section">
+          <div className='input-container'>
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id='submit' onClick={getMessage}>➢</div>
+          </div>
+          <p className='info'>Helio GPT May 22 Version. Free Research Preview.
+            Our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+        </div>
+      </section>
+    
+    </div>
+  )
+}
+
+export default App
